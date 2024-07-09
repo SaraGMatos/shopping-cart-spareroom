fd435b946462c558f1e9d79094886e076a37e362
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-09 11:25:09 +0100

    Implements basic adding behaviour

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-function calculateCartSubtotal() {
-  return 0;
+function calculateCartSubtotal(cart) {
+  let subtotal = 0;
+
+  for (const product of cart) {
+    switch (product.code) {
+      case "A":
+        subtotal += product.quantity * 50;
+        break;
+      case "B":
+        subtotal += product.quantity * 35;
+        break;
+      case "C":
+        subtotal += product.quantity * 25;
+        break;
+      case "D":
+        subtotal += product.quantity * 12;
+    }
+  }
+
+  return subtotal;
 }
 
 module.exports = calculateCartSubtotal;

7b6591bc6e17e2ece436db98bd3f8ae1b4ce291a
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-09 11:52:37 +0100

    Tests that discount formula works adequately

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -39,4 +39,44 @@ describe("calculateCartSubtotal", () => {
 
     expect(calculateCartSubtotal(dataset)).toBe(122);
   });
+
+  test("when given an array of Product Objects where an offer applies, function returns the correct total", () => {
+    const dataset1 = [
+      { code: "A", quantity: 3 },
+      { code: "B", quantity: 1 },
+      { code: "C", quantity: 1 },
+      { code: "D", quantity: 1 },
+    ];
+
+    expect(calculateCartSubtotal(dataset1)).toBe(212);
+
+    const dataset2 = [
+      { code: "A", quantity: 6 },
+      { code: "B", quantity: 1 },
+      { code: "C", quantity: 2 },
+      { code: "D", quantity: 1 },
+    ];
+
+    expect(calculateCartSubtotal(dataset2)).toBe(377);
+  });
+
+  test("when given an array of Product Objects where multiple offers apply, function returns the correct total", () => {
+    const dataset1 = [
+      { code: "A", quantity: 3 },
+      { code: "B", quantity: 2 },
+      { code: "C", quantity: 0 },
+      { code: "D", quantity: 0 },
+    ];
+
+    expect(calculateCartSubtotal(dataset1)).toBe(200);
+
+    const dataset2 = [
+      { code: "A", quantity: 6 },
+      { code: "B", quantity: 4 },
+      { code: "C", quantity: 1 },
+      { code: "D", quantity: 2 },
+    ];
+
+    expect(calculateCartSubtotal(dataset2)).toBe(449);
+  });
 });

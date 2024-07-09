d21a52f369a7ab350e2b3881d2cb505096b9d044
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-09 11:51:42 +0100

    Applies discount formula to relevant products

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
 function calculateCartSubtotal(cart) {
   let subtotal = 0;
+  let normalPrice;
+  let discountedPrice;
+  let minimumItemsForDiscount;
 
   for (const product of cart) {
+    let numOfItemsWithoutOffer;
+    let numOfOffersToApply;
+    let totalWithOfferApplied;
+    let totalWithoutOfferApplied;
+
     switch (product.code) {
       case "A":
-        subtotal += product.quantity * 50;
+        normalPrice = 50;
+        discountedPrice = 140;
+        minimumItemsForDiscount = 3;
+
+        numOfItemsWithoutOffer = product.quantity % minimumItemsForDiscount;
+        numOfOffersToApply =
+          (product.quantity - numOfItemsWithoutOffer) / minimumItemsForDiscount;
+        totalWithOfferApplied = numOfOffersToApply * discountedPrice;
+        totalWithoutOfferApplied = numOfItemsWithoutOffer * normalPrice;
+
+        subtotal += totalWithOfferApplied + totalWithoutOfferApplied;
+
         break;
       case "B":
-        subtotal += product.quantity * 35;
+        normalPrice = 35;
+        discountedPrice = 60;
+        minimumItemsForDiscount = 2;
+
+        numOfItemsWithoutOffer = product.quantity % minimumItemsForDiscount;
+        numOfOffersToApply =
+          (product.quantity - numOfItemsWithoutOffer) / minimumItemsForDiscount;
+        totalWithOfferApplied = numOfOffersToApply * discountedPrice;
+        totalWithoutOfferApplied = numOfItemsWithoutOffer * normalPrice;
+
+        subtotal += totalWithOfferApplied + totalWithoutOfferApplied;
+
         break;
       case "C":
         subtotal += product.quantity * 25;

08d2cd3e451ac54d3c8f0a60dc7386c882acfdcd
SaraGarciaM <saragarciamatos@gmail.com>
2024-07-10 11:41:41 +0100

    Reorganises project

---
+++ src/utils.js
@@ -0,0 +1,52 @@
+/**
+ * Calculates the subtotal of the shopping cart.
+ * @param {object} product The product we are calculating.
+ * @example Product example
+ *  const product = {
+ *    code: "A",
+ *    quantity: 3
+ *  }
+ * @param {number} normalPrice The product's usual price.
+ * @param {number} minimumItemsForDiscount The minimum number of items needed for a discount to apply. For Product A, it will be 3; for Product B, it will be 2. It defaults to 1 for those products with no available offer.
+ * @param {number} specialPrice The price to apply to a particular number of items. For Product A, the special price will be 140 for 3; for Product B, it will be 60 for 2. It defaults to the normal price for those products with no available offer.
+ * @returns {number} A number representing the total for the current product.
+ *
+ * @example Function example
+ * const product = {
+ *    code: "A",
+ *    quantity: 2
+ * };
+ * const normalPrice = 50;
+ * const minimumItemsForDiscount = 3;
+ * const specialPrice = 140;
+ *
+ * const productTotal = calculateProductTotal(product, normalPrice, minimumItemsForDiscount, specialPrice);
+ * console.log(productTotal);
+ * Logs: 100
+ */
+
+function calculateProductTotal(
+  product,
+  normalPrice,
+  minimumItemsForDiscount = 1,
+  specialPrice = normalPrice
+) {
+  let productTotal = 0;
+
+  if (Object.keys(product).length === 0) {
+    return productTotal;
+  }
+
+  const numOfItemsWithoutOffer = product.quantity % minimumItemsForDiscount;
+  const numOfOffersToApply =
+    (product.quantity - numOfItemsWithoutOffer) / minimumItemsForDiscount;
+
+  const totalWithOfferApplied = numOfOffersToApply * specialPrice;
+  const totalWithoutOfferApplied = numOfItemsWithoutOffer * normalPrice;
+
+  productTotal += totalWithOfferApplied + totalWithoutOfferApplied;
+
+  return productTotal;
+}
+
+module.exports = calculateProductTotal;
